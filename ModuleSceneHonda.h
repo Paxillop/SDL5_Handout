9a6416ae95e50e7fe3dcbc3f403139bb38eb970f
Pau Llopart <paxillop@hotmail.es>
2017-03-23 10:44:10 +0100

    praa

---
+++ ModuleSceneHonda.h
@@ -0,0 +1,33 @@
+#ifndef __MODULESCENEHONDA_H__
+#define __MODULESCENEHONDA_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+
+struct SDL_Texture;
+
+class ModuleSceneHonda : public Module
+{
+public:
+	ModuleSceneHonda();
+	~ModuleSceneHonda();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+
+	SDL_Texture* graphics = nullptr;
+	SDL_Rect ground;
+	SDL_Rect roof;
+	SDL_Rect foreground;
+	SDL_Rect background;
+	Animation water;
+	bool fading = false;
+	Module* module_off=nullptr;
+	Module* module_on=nullptr;
+};
+
+#endif // __MODULESCENEHONDA_H__
---
+++ ModuleSceneKen.h
@@ -0,0 +1,36 @@
+#ifndef __MODULESCENEKEN_H__
+#define __MODULESCENEKEN_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+
+struct SDL_Texture;
+
+class ModuleSceneKen : public Module
+{
+public:
+	ModuleSceneKen();
+	~ModuleSceneKen();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+	
+	SDL_Texture* graphics = nullptr;
+	SDL_Rect ground;
+	SDL_Rect foreground;
+	SDL_Rect background;
+	Animation flag;
+	Animation girl;
+
+	float foreground_pos;
+	bool forward;
+	bool fading=false;
+	Module* module_off = nullptr;
+	Module* module_on = nullptr;
+};
+
+#endif // __MODULESCENEKEN_H__
